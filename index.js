30653486128164b99769bc749b09159ef2a7e157
Acstry <59457161+Acstry@users.noreply.github.com>
2021-11-20 12:26:30 -0500

    Node Working

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const fs = require('fs')
-const http = require('http')
-const port = 3000
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('./Webpage/main.html', function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-})
+const server = http.createServer((req, res) => {
+    let filePath = path.join(
+        __dirname,
+        "Webpage",
+        req.url === "/" ? "main.html" : req.url
+    );
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log('something went wrong', error)
-    } else {
-        console.log('server is listening on port' + port)
+    let extName = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extName) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
-})
+
+    console.log(`File path: ${filePath}`);
+    console.log(`Content-Type: ${contentType}`)
+
+    res.writeHead(200, {'Content-Type': contentType});
+
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
+});
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log(`Error: ${err}`)
+    } else {
+        console.log(`Server listening at port ${port}...`);
+    }
+});
