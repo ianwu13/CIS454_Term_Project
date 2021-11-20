3e3545ba21449c31b939436d50d81e4d4ce0628d
Ian Wu <ianwu1099@gmail.com>
2021-11-20 12:07:35 -0500

    node debugging

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const { fstat } = require('fs')
 const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.write('Hello Node')
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fstat.readFile('./Webpage/main.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error) {
